bd0ea3df15c332a501879635820d4b401aebd287
amirhossein yousefi <amirhossein.yousefi1221.com>
2025-08-08 18:01:16 +0330

    going over its own body fixed

--- src/App.tsx
+++ src/App.tsx
@@ -11,70 +11,57 @@ function App() {
     { x: 13, y: 1 },
     { x: 12, y: 1 },
     { x: 11, y: 1 },
-    { x: 10, y: 1 },
-    { x: 9, y: 1 },
   ]);
   useEffect(() => {
     const timer = setInterval(() => {
-      switch (direction) {
-        case "up":
-          setSnake((snake) => {
-            const newSnake = [...snake];
-            const tail = newSnake.pop();
-            const head = newSnake[0];
-            return [{ x: head.x, y: head.y - 1 }, ...newSnake];
-          });
-          break;
-        case "down":
-          setSnake((snake) => {
-            const newSnake = [...snake];
-            const tail = newSnake.pop();
-            const head = newSnake[0];
-            return [{ x: head.x, y: head.y + 1 }, ...newSnake];
-          });
-          break;
-        case "right":
-          setSnake((snake) => {
-            const newSnake = [...snake];
-            const tail = newSnake.pop();
-            const head = newSnake[0];
-            return [{ x: head.x + 1, y: head.y }, ...newSnake];
-          });
-          break;
-        case "left":
-          setSnake((snake) => {
-            const newSnake = [...snake];
-            const tail = newSnake.pop();
-            const head = newSnake[0];
-            return [{ x: head.x - 1, y: head.y }, ...newSnake];
-          });
-          break;
-      }
+      setSnake((snake) => {
+        const newSnake = [...snake];
+        newSnake.pop();
+        const head = newSnake[0];
+        switch (direction) {
+          case "up":
+            return [{ x: head.x, y: (head.y - 1 + 30) % 30 }, ...newSnake];
+          case "down":
+            return [{ x: head.x, y: (head.y + 1 + 30) % 30 }, ...newSnake];
+          case "right":
+            return [{ x: (head.x + 1 + 30) % 30, y: head.y }, ...newSnake];
+          case "left":
+            return [{ x: (head.x - 1 + 30) % 30, y: head.y }, ...newSnake];
+        }
+      });
     }, 300);
     return () => clearInterval(timer);
   }, [direction]);
+
   useEffect(() => {
+    window.addEventListener("keydown", (e) => {
+      if (
+        ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].includes(e.code)
+      ) {
+        e.preventDefault();
+      }
+    });
     const handleKeyDown = (event: KeyboardEvent) => {
       switch (event.key) {
         case "ArrowUp":
-          if (direction !== "up") {
+          if (direction !== "up" && direction !== "down") {
             setDirection("up");
           }
           event.preventDefault();
           break;
         case "ArrowDown":
-          if (direction !== "down") {
+          if (direction !== "down" && direction !== "up") {
             setDirection("down");
           }
           event.preventDefault();
           break;
         case "ArrowLeft":
-          if (direction !== "left") {
+          if (direction !== "left" && direction !== "right") {
             setDirection("left");
           }
           break;
         case "ArrowRight":
-          if (direction !== "right") {
+          if (direction !== "right" && direction !== "left") {
             setDirection("right");
           }
           break;
@@ -82,9 +69,13 @@ function App() {
     };
     window.addEventListener("keydown", handleKeyDown);
     return () => window.removeEventListener("keydown", handleKeyDown);
-  }, []);
+  }, [direction]);
 
-  return <Board apple={apple} snake={snake} />;
+  return (
+    <div className="flex items-center justify-center h-screen w-screen">
+      <Board apple={apple} snake={snake} />;
+    </div>
+  );
 }
 
 export default App;
